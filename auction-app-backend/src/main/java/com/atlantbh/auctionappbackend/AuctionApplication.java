003afdea53fee6c03cfce4a211bff6b25673c2e6
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-02-15 13:30:44 +0100

    Refactor Application class name

--- auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/AuctionApplication.java
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/AuctionApplication.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class AuctionAppBackendApplication {
+public class AuctionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuctionAppBackendApplication.class, args);
+		SpringApplication.run(AuctionApplication.class, args);
 	}
 
 	@GetMapping
