178d34bf5339ed60cc437c0eba46ed2983de84e7
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-02-17 15:53:08 +0100

    Add simple UserRepository test

---
+++ auction-app-backend/src/test/java/com/atlantbh/auctionappbackend/userRepostitoryTest/UserRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.atlantbh.auctionappbackend.userRepostitoryTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.atlantbh.auctionappbackend.domain.User;
+import com.atlantbh.auctionappbackend.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testCreateUser() {
+        User user = new User(
+                "Ajdin",
+                "Becirovic",
+                "email@email.com",
+                "password"
+        );
+
+        User savedUser = userRepository.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+    }
+}
