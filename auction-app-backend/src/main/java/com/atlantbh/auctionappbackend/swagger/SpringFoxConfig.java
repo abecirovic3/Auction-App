f5f539b2a9c570f661877dfa22a2cf96db9b94fc
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-02-21 11:30:32 +0100

    Create separate package for swagger config

--- auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/swagger/SpringFoxConfig.java
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/swagger/SpringFoxConfig.java
@@ -1,4 +1,4 @@
-package com.atlantbh.auctionappbackend;
+package com.atlantbh.auctionappbackend.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
