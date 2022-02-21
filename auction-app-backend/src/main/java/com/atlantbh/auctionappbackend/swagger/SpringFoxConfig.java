3b98593b13366b24aa818872e05aa92c310c552e
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-02-21 14:54:07 +0100

    Add swagger API info

--- auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/swagger/SpringFoxConfig.java
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/swagger/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.atlantbh.auctionappbackend.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -12,9 +15,20 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("AuctionApp API")
+                .description("Auction App API reference for developers")
+                .contact(new Contact(
+                        "Ajdin Becirovic",
+                        "https://github.com/abecirovic3/Auction-App",
+                        "abecirovic3@etf.unsa.ba"))
                 .build();
     }
 }
