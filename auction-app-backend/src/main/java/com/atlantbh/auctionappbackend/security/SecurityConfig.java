86cbcf0d380167fdee8bc5b5440f6ba6ad929d28
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-02-23 09:07:28 +0100

    Disable csrf

--- auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/security/SecurityConfig.java
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/security/SecurityConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -51,17 +50,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter =
-                new CustomAuthenticationFilter(authenticationManagerBean(), jwtConfig, signAlgorithm);
+                new CustomAuthenticationFilter(
+                                                authenticationManagerBean(),
+                                                jwtConfig,
+                                                signAlgorithm
+                );
         customAuthenticationFilter.setFilterProcessesUrl(apiPrefix + "/user/login");
 
-        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         http.authorizeRequests()
                 .antMatchers(AuthWhitelistConfig.getAuthWhitelist()).permitAll();
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(
-                new CustomAuthorizationFilter(jwtConfig, signAlgorithm), UsernamePasswordAuthenticationFilter.class
+                                new CustomAuthorizationFilter(jwtConfig, signAlgorithm),
+                                UsernamePasswordAuthenticationFilter.class
         );
     }
 
