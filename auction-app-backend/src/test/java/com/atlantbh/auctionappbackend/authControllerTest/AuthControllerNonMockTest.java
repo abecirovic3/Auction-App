f330faf2fe8aa11c015293afde9e4b7b00300d77
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-02-24 17:30:45 +0100

    Add AuthController tests

---
+++ auction-app-backend/src/test/java/com/atlantbh/auctionappbackend/authControllerTest/AuthControllerNonMockTest.java
@@ -0,0 +1,138 @@
+package com.atlantbh.auctionappbackend.authControllerTest;
+
+import com.atlantbh.auctionappbackend.domain.User;
+import com.atlantbh.auctionappbackend.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AuthControllerNonMockTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void destroyAll(){
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/v1/health",
+                String.class)).contains("OK");
+    }
+
+    @Test
+    public void testRegisterUserSuccess() {
+        User user = new User(1L,"Foo", "Bar", "foo@bar.org.com", "password", "ROLE_USER");
+        String resp = this.restTemplate.postForObject(
+                "http://localhost:" + port + "/api/v1/auth/register",
+                user,
+                String.class
+        );
+        assertThat(resp.contains("\"firstName\":\"Foo\",\"lastName\":\"Bar\",\"email\":\"foo@bar.org.com\"")).isTrue();
+    }
+
+    @Test
+    public void testRegisterUserInvalidRequest() {
+        User user = new User(1L,"Foo", "Bar", "foo@bar.org.com", "password", "ROLE_USER");
+        user.setFirstName("");
+        String resp = this.restTemplate.postForObject(
+                "http://localhost:" + port + "/api/v1/auth/register",
+                user,
+                String.class
+        );
+
+        assertThat(resp.contains("\"firstName\"")).isTrue();
+    }
+
+    @Test
+    public void testLoginUserSuccess() {
+        User user = new User(1L,"Foo", "Bar", "foo@bar.org.com", "password", "ROLE_USER");
+        this.restTemplate.postForObject(
+                "http://localhost:" + port + "/api/v1/auth/register",
+                user,
+                String.class
+        );
+
+        String resp = this.restTemplate.postForObject(
+                "http://localhost:" + port + "/api/v1/auth/login?email=" + user.getEmail() + "&password=" + user.getPassword(),
+                null,
+                String.class
+        );
+
+        assertThat(resp.contains("\"access_token\"")).isTrue();
+        assertThat(resp.contains("\"refresh_token\"")).isTrue();
+    }
+
+    @Test
+    public void testLoginUserFail() {
+        User user = new User(1L,"Foo", "Bar", "foo@bar.org.com", "password", "ROLE_USER");
+
+        String resp = this.restTemplate.postForObject(
+                "http://localhost:" + port + "/api/v1/auth/login?email=" + user.getEmail() + "&password=pogresan",
+                null,
+                String.class
+        );
+
+        assertThat(resp).isEqualTo("{\"error\":\"Failed Authentication, Bad Credentials\"}");
+    }
+
+    @Test
+    public void testRefreshTokenSuccess() {
+        User user = new User(1L,"Foo", "Bar", "foo@bar.org.com", "password", "ROLE_USER");
+        this.restTemplate.postForObject(
+                "http://localhost:" + port + "/api/v1/auth/register",
+                user,
+                String.class
+        );
+
+        String resp = this.restTemplate.postForObject(
+                "http://localhost:" + port + "/api/v1/auth/login?email=" + user.getEmail() + "&password=" + user.getPassword(),
+                null,
+                String.class
+        );
+
+        int startIndex = resp.indexOf("\"refresh_token\"");
+        String refresh_token = resp.substring(startIndex + "\"refresh_token\":\"".length(), resp.length() - 2);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(refresh_token);
+        HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+        ResponseEntity<String> finalResponse = restTemplate.exchange(
+                "http://localhost:" + port + "/api/v1/auth/token/refresh",
+                HttpMethod.GET,
+                entity,
+                String.class
+        );
+
+        assertThat(finalResponse.getBody()).isNotNull();
+        assertThat(finalResponse.getBody().contains("\"access_token\"")).isTrue();
+        assertThat(finalResponse.getBody().contains("\"refresh_token\"")).isTrue();
+
+    }
+
+}
---
+++ auction-app-backend/src/test/java/com/atlantbh/auctionappbackend/authControllerTest/AuthControllerTest.java
@@ -0,0 +1,69 @@
+package com.atlantbh.auctionappbackend.authControllerTest;
+
+import com.atlantbh.auctionappbackend.api.ApiConfig;
+import com.atlantbh.auctionappbackend.controller.AuthController;
+import com.atlantbh.auctionappbackend.domain.User;
+import com.atlantbh.auctionappbackend.security.JwtConfig;
+import com.atlantbh.auctionappbackend.security.JwtUtil;
+import com.atlantbh.auctionappbackend.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.servlet.Filter;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AuthController.class)
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+    @MockBean
+    private JwtUtil jwtUtil;
+    @MockBean
+    private JwtConfig jwtConfig;
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+    @MockBean
+    private ApiConfig apiConfig;
+    @MockBean(name = "springSecurityFilterChain")
+    private Filter springSecurityFilterChain;
+
+
+    @Test
+    public void greetingShouldReturnMessageFromService() throws Exception {
+        User user = new User(1L,"Foo", "Bar", "foo@bar.org.com", "password", "ROLE_USER");
+        when(userService.registerUser(any())).thenReturn(user);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/auth/register")
+                                .content(asJsonString(user))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().string(asJsonString(user)));
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
