f69183f17fd424596a4acd5f422719c6b9c95e14
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-02-25 15:41:20 +0100

    Implement AuthController test

--- auction-app-backend/src/test/java/com/atlantbh/auctionappbackend/controller/AuthControllerTest.java
+++ auction-app-backend/src/test/java/com/atlantbh/auctionappbackend/controller/AuthControllerTest.java
@@ -3,11 +3,13 @@ package com.atlantbh.auctionappbackend.controller;
 import com.atlantbh.auctionappbackend.api.ApiConfig;
 import com.atlantbh.auctionappbackend.domain.User;
 import com.atlantbh.auctionappbackend.security.JwtConfig;
-import com.atlantbh.auctionappbackend.security.JwtUtil;
 import com.atlantbh.auctionappbackend.service.UserService;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -17,12 +19,15 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.servlet.Filter;
 
+import static com.atlantbh.auctionappbackend.security.ApplicationUserRole.USER;
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class AuthControllerTest {
 
     @Autowired
@@ -31,7 +36,7 @@ public class AuthControllerTest {
     @MockBean
     private UserService userService;
     @MockBean
-    private JwtUtil jwtUtil;
+    private Algorithm signAlgorithm;
     @MockBean
     private JwtConfig jwtConfig;
     @MockBean
@@ -41,11 +46,50 @@ public class AuthControllerTest {
     @MockBean(name = "springSecurityFilterChain")
     private Filter springSecurityFilterChain;
 
+    private final User user = new User();
+
+    @BeforeEach
+    public void initUser() {
+        user.setId(1L);
+        user.setFirstName("Foo");
+        user.setLastName("Bar");
+        user.setEmail("foo@bar.org.com");
+        user.setPassword("password");
+        user.setRole(USER.getRole());
+    }
+
 
     @Test
-    public void greetingShouldReturnMessageFromService() throws Exception {
-        User user = new User(1L,"Foo", "Bar", "foo@bar.org.com", "password", "ROLE_USER");
+    public void testRegisterUserSuccess() throws Exception {
         when(userService.registerUser(any())).thenReturn(user);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/v1/auth/register")
+                        .content(asJsonString(user))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().string(asJsonString(user)));
+    }
+
+    @Test
+    public void testRegisterUserInvalidFirstName() throws Exception {
+        user.setFirstName(null);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/v1/auth/register")
+                        .content(asJsonString(user))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("\"firstName\"")));
+    }
+
+    @Test
+    public void testRegisterUserInvalidLastName() throws Exception {
+        user.setLastName("Invalid1234");
 
         mockMvc.perform(
                         MockMvcRequestBuilders.post("/api/v1/auth/register")
@@ -53,8 +97,38 @@ public class AuthControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .accept(MediaType.APPLICATION_JSON)
                 )
-                .andExpect(status().isOk())
-                .andExpect(content().string(asJsonString(user)));
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("\"lastName\"")));
+    }
+
+    @Test
+    public void testRegisterUserInvalidEmail() throws Exception {
+        user.setEmail("foo@not_valid");
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/auth/register")
+                                .content(asJsonString(user))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("\"email\"")));
+    }
+
+    @Test
+    public void testRegisterUserInvalidEmailAndPassword() throws Exception {
+        user.setEmail("foo@notValid");
+        user.setPassword("");
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/auth/register")
+                                .content(asJsonString(user))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(containsString("\"password\"")))
+                .andExpect(content().string(containsString("\"email\"")));
     }
 
     private String asJsonString(final Object obj) {
@@ -64,5 +138,6 @@ public class AuthControllerTest {
             throw new RuntimeException(e);
         }
     }
+
 
 }
