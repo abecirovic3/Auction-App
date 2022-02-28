d6386ac6487eeb2ecd28e42ae051a43b414a123e
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-02-28 14:31:15 +0100

    Create basic Forgot Password form

--- auction-app-frontend/src/App.js
+++ auction-app-frontend/src/App.js
@@ -1,9 +1,11 @@
 import Register from "./components/Register/Register"
 import Login from "./components/Login/Login";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 
 function App() {
   return (
     <div className="App">
+        <ForgotPassword />
         <Login />
         <Register />
     </div>
---
+++ auction-app-frontend/src/components/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,44 @@
+import {createTheme, StyledEngineProvider, ThemeProvider} from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
+import facebookIcon from "../../img/facebook.svg";
+import googleIcon from "../../img/google.svg";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#8367D8"
+        }
+    }
+});
+
+const ForgotPassword = () => {
+    return (
+        <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+                <Container className="form-container" maxWidth="sm">
+                    <h5>FORGOT PASSWORD</h5>
+
+                    <Stack width="80%" margin="auto" spacing={5} paddingBottom={5}>
+                        <Stack spacing={2}>
+                            <p id="lostPasswordMsg">
+                                Lost your password? Please enter your username or email address.
+                                You will receive a link to create a new password via email.
+                            </p>
+                            <label htmlFor="email">Enter Email</label>
+                            <TextField id="email" variant="outlined" type="email" />
+                        </Stack>
+
+                        <Button color={"primary"} variant="contained">Reset Password</Button>
+                    </Stack>
+                </Container>
+            </ThemeProvider>
+        </StyledEngineProvider>
+    );
+};
+
+export default ForgotPassword;
