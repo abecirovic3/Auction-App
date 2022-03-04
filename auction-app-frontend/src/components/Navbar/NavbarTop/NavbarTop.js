eef8e6186d93d8391267ba98936d537377141170
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-04 19:50:55 +0100

    Extract social media buttons to component

--- auction-app-frontend/src/components/Navbar/NavbarTop/NavbarTop.js
+++ auction-app-frontend/src/components/Navbar/NavbarTop/NavbarTop.js
@@ -1,10 +1,7 @@
-import { IconButton } from '@mui/material';
 import { StyledEngineProvider } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 
-import facebookIcon from '../../../assets/img/navbarFacebook.svg';
-import instagramIcon from '../../../assets/img/navbarInstagram.svg';
-import twitterIcon from '../../../assets/img/navbarTwitter.svg';
+import SocialMediaBtnContainer from '../../SocialMediaBtnContainer/SocialMediaBtnContainer';
 
 import '../../../assets/style/navbar-top.scss'
 
@@ -12,17 +9,8 @@ const NavbarTop = () => {
     return (
         <StyledEngineProvider injectFirst>
             <div className='navbar-top-container'>
-                <div>
-                    <IconButton onClick={() => {window.open('https://www.facebook.com/AtlantBH'); }}>
-                        <img src={facebookIcon} alt='Facebook' />
-                    </IconButton>
-                    <IconButton onClick={() => {window.open('https://www.instagram.com/atlantbh/?hl=en')}}>
-                        <img src={instagramIcon} alt='Instagram' />
-                    </IconButton>
-                    <IconButton onClick={() => {window.open('https://twitter.com/atlantbh?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor')}}>
-                        <img src={twitterIcon} alt='Twitter' />
-                    </IconButton>
-                </div>
+                <SocialMediaBtnContainer />
+                {/*TODO this will need to be dynamic depending on user login*/}
                 <p> <Link to='/login'>Login</Link> or <Link to='/register'>Create an account</Link></p>
             </div>
         </StyledEngineProvider>
