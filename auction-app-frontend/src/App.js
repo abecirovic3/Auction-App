e8c43b41c4dcaba14c4d328d1c5720b4881c996b
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-05 12:10:02 +0100

    Create main page layout and make navbar stick to top of page

--- auction-app-frontend/src/App.js
+++ auction-app-frontend/src/App.js
@@ -10,12 +10,19 @@ function App() {
   return (
       <BrowserRouter>
           <Navbar />
-          <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/login' element={<Login />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/forgot-password' element={<ForgotPassword />} />
-          </Routes>
+          <div className='main-page-container'>
+              <div className='main-page-content-container'>
+                  <Routes>
+                      <Route path='/' element={<Home />} />
+                      <Route path='/login' element={<Login />} />
+                      <Route path='/register' element={<Register />} />
+                      <Route path='/forgot-password' element={<ForgotPassword />} />
+                  </Routes>
+              </div>
+              <div>
+                  <h1>Footer</h1>
+              </div>
+          </div>
       </BrowserRouter>
   );
 }
--- auction-app-frontend/src/components/Navbar/Navbar.js
+++ auction-app-frontend/src/components/Navbar/Navbar.js
@@ -1,12 +1,19 @@
+import { AppBar } from '@mui/material';
 import NavbarTop from './NavbarTop/NavbarTop';
 import NavbarBottom from './NavbarBottom/NavbarBottom';
 
 const Navbar = () => {
     return (
-        <>
+        <AppBar
+            position='fixed'
+            sx={{
+                boxShadow: 'none',
+                backgroundColor: 'white'
+            }}
+        >
             <NavbarTop />
             <NavbarBottom />
-        </>
+        </AppBar>
     );
 };
 
