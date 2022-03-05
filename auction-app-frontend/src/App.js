966ec9460b57df319f46eb754fc820c5cb0214c8
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-05 14:52:39 +0100

    Implement basic static pages

--- auction-app-frontend/src/App.js
+++ auction-app-frontend/src/App.js
@@ -6,6 +6,9 @@ import Login from './components/Login/Login';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import PrivacyAndPolicy from './components/PricacyAndPolicy/PrivacyAndPolicy';
+import TermsAndConditions from './components/TermsAndConditions/TermsAndConditions';
+import About from './components/About/About';
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
                       <Route path='/login' element={<Login />} />
                       <Route path='/register' element={<Register />} />
                       <Route path='/forgot-password' element={<ForgotPassword />} />
+                      <Route path='/privacy' element={<PrivacyAndPolicy />} />
+                      <Route path='/terms' element={<TermsAndConditions />} />
+                      <Route path='/about' element={<About />} />
                   </Routes>
               </div>
               <Footer />
---
+++ auction-app-frontend/src/components/TermsAndConditions/TermsAndConditions.js
@@ -0,0 +1,18 @@
+import '../../assets/style/static-page.scss'
+
+const TermsAndConditions = () => {
+    return (
+        <div className='static-page-container'>
+            <div className='text-container custom-width'>
+                <h3 className='heading-1'>Some title here</h3>
+                <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis optio consequuntur fugiat earum vero autem et blanditiis illum iste inventore, dolores, dignissimos ab nobis reiciendis culpa totam explicabo asperiores fugit amet iure quo, assumenda sed accusantium. Ratione, culpa blanditiis? Cumque consectetur expedita, excepturi laborum odio maxime unde amet exercitationem, corporis asperiores nesciunt eveniet veniam perferendis non, alias facere ad vero adipisci perspiciatis est necessitatibus placeat doloremque nihil. Sequi excepturi quidem magnam maxime incidunt facere animi enim atque, dicta necessitatibus provident magni eveniet eum nemo culpa adipisci maiores saepe veniam dolores cumque in laboriosam nam reiciendis rerum! Totam animi fuga vel?</p>
+                <h3 className='heading-2'>Some title here</h3>
+                <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dicta, libero inventore ullam corrupti incidunt numquam dignissimos nesciunt eaque consequuntur, aliquid enim, amet quisquam fugit sint deserunt quam soluta nulla laboriosam reiciendis veniam optio omnis quia. Voluptatum laborum ipsum hic dolore eius ratione exercitationem enim odio excepturi tempora temporibus fugiat possimus aperiam aut voluptates, quo ipsa! Harum vitae reprehenderit deleniti, rerum corrupti in debitis, repellendus quaerat eligendi cumque ea modi!</p>
+                <h3 className='heading-2'>Some title here</h3>
+                <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae architecto praesentium voluptates perspiciatis voluptate expedita nobis quis soluta reiciendis mollitia ducimus quaerat officia quisquam vel, quo, nisi sunt ullam nulla accusantium cum laboriosam saepe voluptatibus nihil unde. Eaque nostrum facere ipsa. Amet temporibus animi rerum voluptates tempore. Blanditiis, consequuntur fuga.</p>
+            </div>
+        </div>
+    );
+};
+
+export default TermsAndConditions;
