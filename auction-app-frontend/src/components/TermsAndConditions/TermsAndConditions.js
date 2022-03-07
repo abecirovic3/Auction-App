e09cc31e756ac956434e76c1e723c78c4e5d093d
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-07 10:34:59 +0100

    Use generated text for Terms and Conditions

--- auction-app-frontend/src/components/TermsAndConditions/TermsAndConditions.js
+++ auction-app-frontend/src/components/TermsAndConditions/TermsAndConditions.js
@@ -5,12 +5,86 @@ const TermsAndConditions = () => {
         <div className='static-page-container'>
             <div className="static-page-content-container">
                 <div className='text-container custom-width'>
-                    <h3 className='heading-1'>Some title here</h3>
-                    <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis optio consequuntur fugiat earum vero autem et blanditiis illum iste inventore, dolores, dignissimos ab nobis reiciendis culpa totam explicabo asperiores fugit amet iure quo, assumenda sed accusantium. Ratione, culpa blanditiis? Cumque consectetur expedita, excepturi laborum odio maxime unde amet exercitationem, corporis asperiores nesciunt eveniet veniam perferendis non, alias facere ad vero adipisci perspiciatis est necessitatibus placeat doloremque nihil. Sequi excepturi quidem magnam maxime incidunt facere animi enim atque, dicta necessitatibus provident magni eveniet eum nemo culpa adipisci maiores saepe veniam dolores cumque in laboriosam nam reiciendis rerum! Totam animi fuga vel?</p>
-                    <h3 className='heading-2'>Some title here</h3>
-                    <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dicta, libero inventore ullam corrupti incidunt numquam dignissimos nesciunt eaque consequuntur, aliquid enim, amet quisquam fugit sint deserunt quam soluta nulla laboriosam reiciendis veniam optio omnis quia. Voluptatum laborum ipsum hic dolore eius ratione exercitationem enim odio excepturi tempora temporibus fugiat possimus aperiam aut voluptates, quo ipsa! Harum vitae reprehenderit deleniti, rerum corrupti in debitis, repellendus quaerat eligendi cumque ea modi!</p>
-                    <h3 className='heading-2'>Some title here</h3>
-                    <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae architecto praesentium voluptates perspiciatis voluptate expedita nobis quis soluta reiciendis mollitia ducimus quaerat officia quisquam vel, quo, nisi sunt ullam nulla accusantium cum laboriosam saepe voluptatibus nihil unde. Eaque nostrum facere ipsa. Amet temporibus animi rerum voluptates tempore. Blanditiis, consequuntur fuga.</p>
+                    {/*<h3 className='heading-1'>Some title here</h3>*/}
+                    <h1 className='heading-1'>Terms and Conditions for Auction Company</h1>
+
+                    <h2 className='heading-2'>Introduction</h2>
+
+                    <p className='text'>These Website Standard Terms and Conditions written on this webpage shall manage your use of our website, Acution accessible at http:auctionapp.com.</p>
+
+                    <p className='text'>These Terms will be applied fully and affect to your use of this Website. By using this Website, you agreed to accept all terms and conditions written in here. You must not use this Website if you disagree with any of these Website Standard Terms and Conditions. These Terms and Conditions have been generated with the help of the <a href="https://www.termsandcondiitionssample.com">Terms And Conditiions Sample Generator</a>.</p>
+
+                    <p className='text'>Minors or people below 18 years old are not allowed to use this Website.</p>
+
+                    <h2 className='heading-2'>Intellectual Property Rights</h2>
+
+                    <p className='text'>Other than the content you own, under these Terms, Auction Company and/or its licensors own all the intellectual property rights and materials contained in this Website.</p>
+
+                    <p className='text'>You are granted limited license only for purposes of viewing the material contained on this Website.</p>
+
+                    <h2 className='heading-2'>Restrictions</h2>
+
+                    <p className='text'>You are specifically restricted from all of the following:</p>
+
+                    <ul className='list'>
+                        <li>publishing any Website material in any other media;</li>
+                        <li>selling, sublicensing and/or otherwise commercializing any Website material;</li>
+                        <li>publicly performing and/or showing any Website material;</li>
+                        <li>using this Website in any way that is or may be damaging to this Website;</li>
+                        <li>using this Website in any way that impacts user access to this Website;</li>
+                        <li>using this Website contrary to applicable laws and regulations, or in any way may cause harm to the Website, or to any person or business entity;</li>
+                        <li>engaging in any data mining, data harvesting, data extracting or any other similar activity in relation to this Website;</li>
+                        <li>using this Website to engage in any advertising or marketing.</li>
+                    </ul>
+
+                    <p className='text'>Certain areas of this Website are restricted from being access by you and Auction Company may further restrict access by you to any areas of this Website, at any time, in absolute discretion. Any user ID and password you may have for this Website are confidential and you must maintain confidentiality as well.</p>
+
+                    <h2 className='heading-2'>Your Content</h2>
+
+                    <p className='text'>In these Website Standard Terms and Conditions, "Your Content" shall mean any audio, video text, images or other material you choose to display on this Website. By displaying Your Content, you grant Auction Company a non-exclusive, worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, translate and distribute it in any and all media.</p>
+
+                    <p className='text'>Your Content must be your own and must not be invading any third-party’s rights. Auction Company reserves the right to remove any of Your Content from this Website at any time without notice.</p>
+
+                    <h2 className='heading-2'>Your Privacy</h2>
+
+                    <p className='text'>Please read Privacy Policy.</p>
+
+                    <h2 className='heading-2'>No warranties</h2>
+
+                    <p className='text'>This Website is provided "as is," with all faults, and Auction Company express no representations or warranties, of any kind related to this Website or the materials contained on this Website. Also, nothing contained on this Website shall be interpreted as advising you.</p>
+
+                    <h2 className='heading-2'>Limitation of liability</h2>
+
+                    <p className='text'>In no event shall Auction Company, nor any of its officers, directors and employees, shall be held liable for anything arising out of or in any way connected with your use of this Website whether such liability is under contract.  Auction Company, including its officers, directors and employees shall not be held liable for any indirect, consequential or special liability arising out of or in any way related to your use of this Website.</p>
+
+                    <h2 className='heading-2'>Indemnification</h2>
+
+                    <p className='text'>You hereby indemnify to the fullest extent Auction Company from and against any and/or all liabilities, costs, demands, causes of action, damages and expenses arising in any way related to your breach of any of the provisions of these Terms.</p>
+
+                    <h2 className='heading-2'>Severability</h2>
+
+                    <p className='text'>If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.</p>
+
+                    <h2 className='heading-2'>Variation of Terms</h2>
+
+                    <p className='text'>Auction Company is permitted to revise these Terms at any time as it sees fit, and by using this Website you are expected to review these Terms on a regular basis.</p>
+
+                    <h2 className='heading-2'>Assignment</h2>
+
+                    <p className='text'>The Auction Company is allowed to assign, transfer, and subcontract its rights and/or obligations under these Terms without any notification. However, you are not allowed to assign, transfer, or subcontract any of your rights and/or obligations under these Terms.</p>
+
+                    <h2 className='heading-2'>Entire Agreement</h2>
+
+                    <p className='text'>These Terms constitute the entire agreement between Auction Company and you in relation to your use of this Website, and supersede all prior agreements and understandings.</p>
+
+                    <h2 className='heading-2'>Governing Law & Jurisdiction</h2>
+
+                    <p className='text'>These Terms will be governed by and interpreted in accordance with the laws of the State of ba, and you submit to the non-exclusive jurisdiction of the state and federal courts located in ba for the resolution of any disputes.</p>
+                    {/*<p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis optio consequuntur fugiat earum vero autem et blanditiis illum iste inventore, dolores, dignissimos ab nobis reiciendis culpa totam explicabo asperiores fugit amet iure quo, assumenda sed accusantium. Ratione, culpa blanditiis? Cumque consectetur expedita, excepturi laborum odio maxime unde amet exercitationem, corporis asperiores nesciunt eveniet veniam perferendis non, alias facere ad vero adipisci perspiciatis est necessitatibus placeat doloremque nihil. Sequi excepturi quidem magnam maxime incidunt facere animi enim atque, dicta necessitatibus provident magni eveniet eum nemo culpa adipisci maiores saepe veniam dolores cumque in laboriosam nam reiciendis rerum! Totam animi fuga vel?</p>*/}
+                    {/*<h3 className='heading-2'>Some title here</h3>*/}
+                    {/*<p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dicta, libero inventore ullam corrupti incidunt numquam dignissimos nesciunt eaque consequuntur, aliquid enim, amet quisquam fugit sint deserunt quam soluta nulla laboriosam reiciendis veniam optio omnis quia. Voluptatum laborum ipsum hic dolore eius ratione exercitationem enim odio excepturi tempora temporibus fugiat possimus aperiam aut voluptates, quo ipsa! Harum vitae reprehenderit deleniti, rerum corrupti in debitis, repellendus quaerat eligendi cumque ea modi!</p>*/}
+                    {/*<h3 className='heading-2'>Some title here</h3>*/}
+                    {/*<p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae architecto praesentium voluptates perspiciatis voluptate expedita nobis quis soluta reiciendis mollitia ducimus quaerat officia quisquam vel, quo, nisi sunt ullam nulla accusantium cum laboriosam saepe voluptatibus nihil unde. Eaque nostrum facere ipsa. Amet temporibus animi rerum voluptates tempore. Blanditiis, consequuntur fuga.</p>*/}
                 </div>
             </div>
         </div>
