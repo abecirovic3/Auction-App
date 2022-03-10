400c13e959575e3c690c4d679d303c4701b00f2d
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-10 12:18:44 +0100

    Implement basic home product grid layout

--- auction-app-frontend/src/components/Home/Home.js
+++ auction-app-frontend/src/components/Home/Home.js
@@ -1,9 +1,13 @@
 import HomeMain from 'components/Home/HomeMain/HomeMain';
+import HomeProductsTabContainer from 'components/Home/HomeItemsTabContainer/HomeProductsTabContainer';
+
+import 'assets/style/home-page.scss';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home-page-container'>
             <HomeMain />
+            <HomeProductsTabContainer />
         </div>
     );
 };
