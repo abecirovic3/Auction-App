cf15db0190f3545fbefb14eb1260504b2d216864
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-10 15:44:06 +0100

    Create simple Product entity

---
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/domain/Product.java
@@ -0,0 +1,144 @@
+package com.atlantbh.auctionappbackend.domain;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity(name = "Product")
+@Table(name = "product")
+public class Product {
+    @Id
+    @SequenceGenerator(
+            name = "product_sequence",
+            sequenceName = "product_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_sequence"
+    )
+    @Column(updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Double startPrice;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    private Integer size;
+    private String color;
+
+    public Product() {
+        // No args constructor needed by **framework**
+    }
+
+    public Product(String name, String description, Double startPrice, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.description = description;
+        this.startPrice = startPrice;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Product(Long id, String name, String description, Double startPrice, LocalDate startDate, LocalDate endDate) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.startPrice = startPrice;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Product(
+                    Long id,
+                    String name,
+                    String description,
+                    Double startPrice,
+                    LocalDate startDate,
+                    LocalDate endDate,
+                    Integer size,
+                    String color
+    ) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.startPrice = startPrice;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.size = size;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getStartPrice() {
+        return startPrice;
+    }
+
+    public void setStartPrice(Double startPrice) {
+        this.startPrice = startPrice;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
