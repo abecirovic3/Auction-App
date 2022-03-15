651b57dd96896fdaeda1af904adaf6ffb5f437f9
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-15 11:29:19 +0100

    Extract product cover image style

--- auction-app-frontend/src/components/Product/Product.js
+++ auction-app-frontend/src/components/Product/Product.js
@@ -3,7 +3,7 @@ import 'assets/style/product.scss';
 const Product = ({ name, startPrice, img, imgAlt }) => {
     return (
         <div className='product-container'>
-            <img style={{maxHeight: '350px', maxWidth: '100%', overflow: 'hidden'}} src={img} alt={imgAlt}/>
+            <img className='cover-img' src={img} alt={imgAlt}/>
             <div className='product-details'>
                 <h3 className='name'>{name}</h3>
                 <p className='start-price-label'>Start from</p>
