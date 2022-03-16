9175d94e267fe4d99445cff5254d4cf304b863ef
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-16 12:30:50 +0100

    Add product overview route

--- auction-app-frontend/src/configuration/RoutesConfig.js
+++ auction-app-frontend/src/configuration/RoutesConfig.js
@@ -3,4 +3,6 @@ export const routes = [
     { path: '/privacy', breadcrumb: 'Privacy and Policy' },
     { path: '/terms', breadcrumb: 'Terms and Conditions' },
     { path: '/about', breadcrumb: 'About Us' },
+    { path: '/shop', breadcrumb: 'Shop' },
+    { path: '/shop/product-overview', breadcrumb: 'Single product' },
 ];
