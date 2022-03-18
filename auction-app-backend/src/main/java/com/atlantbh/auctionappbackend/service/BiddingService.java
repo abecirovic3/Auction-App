257076f2df9efade0c45523f0ef035ca9124cd14
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-18 14:58:14 +0100

    Fetch product from db for validation on bid registration

--- auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/service/BiddingService.java
+++ auction-app-backend/src/main/java/com/atlantbh/auctionappbackend/service/BiddingService.java
@@ -1,6 +1,8 @@
 package com.atlantbh.auctionappbackend.service;
 
+import com.atlantbh.auctionappbackend.domain.Product;
 import com.atlantbh.auctionappbackend.domain.ProductUserBid;
+import com.atlantbh.auctionappbackend.repository.ProductRepository;
 import com.atlantbh.auctionappbackend.repository.ProductUserBidRepository;
 import com.atlantbh.auctionappbackend.response.BidInfoResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +16,27 @@ import java.util.Optional;
 @Service
 public class BiddingService {
     private final ProductUserBidRepository productUserBidRepository;
+    private final ProductRepository productRepository;
 
     @Autowired
-    public BiddingService(ProductUserBidRepository productUserBidRepository) {
+    public BiddingService(ProductUserBidRepository productUserBidRepository, ProductRepository productRepository) {
         this.productUserBidRepository = productUserBidRepository;
+        this.productRepository = productRepository;
     }
 
     public BidInfoResponse processBid(ProductUserBid bid) {
-        if (bid.getProduct().getSeller().getId().equals(bid.getUser().getId())) {
+        Optional<Product> productOptional = productRepository.findById(bid.getProduct().getId());
+
+        if (productOptional.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Product with " + bid.getProduct().getId() + " doesn't exist"
+            );
+        }
+
+        Product product = productOptional.get();
+
+        if (product.getSeller().getId().equals(bid.getUser().getId())) {
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST,
                     "Seller can not bid on his/her products"
@@ -29,10 +44,10 @@ public class BiddingService {
         }
 
         Optional<ProductUserBid> higherBidOptional =
-                productUserBidRepository.findByProductAndAmountGreaterThanEqual(bid.getProduct(), bid.getAmount());
+                productUserBidRepository.findByProductAndAmountGreaterThanEqual(product, bid.getAmount());
         if (higherBidOptional.isPresent()) {
             return new BidInfoResponse("fail", "There are higher bids than yours. You could give a second try!");
-        } else if (bid.getAmount() <= bid.getProduct().getStartPrice()) {
+        } else if (bid.getAmount() <= product.getStartPrice()) {
             return new BidInfoResponse("fail", "Bid must be higher than start price. You could give a second try!");
         }
 
