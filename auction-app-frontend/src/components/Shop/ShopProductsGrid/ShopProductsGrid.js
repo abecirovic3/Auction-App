617269837ccf43c8d9abe472a6ed1ec4cb7a4908
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-24 19:56:59 +0100

    Add style to sort and layout selector

--- auction-app-frontend/src/components/Shop/ShopProductsGrid/ShopProductsGrid.js
+++ auction-app-frontend/src/components/Shop/ShopProductsGrid/ShopProductsGrid.js
@@ -1,46 +1,68 @@
 import { useState } from 'react';
 import { Select, MenuItem, Grid, Button } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 
 import gridGrayIcon from 'assets/img/grid-gray.png';
 import gridPurpleIcon from 'assets/img/grid-purple.png';
 import listGrayIcon from 'assets/img/list-gray.png';
 import listPurpleIcon from 'assets/img/list-purple.png';
 
+import MainTheme from 'themes/MainTheme';
+import 'assets/style/shop-product-grid.scss';
+
 const ShopProductsGrid = () => {
     const [itemWidth, setItemWidth] = useState(4);
 
     return (
-        <div className='shop-products-grid-container'>
-            <div>
-                <Select value={1}>
-                    <MenuItem value={1}>Default Sorting</MenuItem>
-                    <MenuItem value={2}>Other Sorting</MenuItem>
-                    <MenuItem value={3}>Some Other Sorting</MenuItem>
-                </Select>
-                <Button startIcon={<img src={gridPurpleIcon} alt='grid' />} onClick={() => {setItemWidth(4)}}>Grid</Button>
-                <Button startIcon={<img src={listGrayIcon} alt='list' />} onClick={() => {setItemWidth(12)}}>List</Button>
+        <ThemeProvider theme={MainTheme}>
+            <div className='shop-products-grid-container'>
+                <div className='sort-and-layout-selector'>
+                    <Select className='sort-select' value={1}>
+                        <MenuItem value={1}>Default Sorting</MenuItem>
+                        <MenuItem value={2}>Other Sorting</MenuItem>
+                        <MenuItem value={3}>Some Other Sorting</MenuItem>
+                    </Select>
+                    <div>
+                        <Button
+                            id='grid-layout-btn'
+                            startIcon={<img src={itemWidth === 4 ? gridPurpleIcon : gridGrayIcon} alt='grid' />}
+                            color={itemWidth === 4 ? 'primary' : 'secondary'}
+                            onClick={() => {setItemWidth(4)}}
+                        >
+                            Grid
+                        </Button>
+                        <Button
+                            id='list-layout-btn'
+                            startIcon={<img src={itemWidth === 12 ? listPurpleIcon : listGrayIcon} alt='list' />}
+                            color={itemWidth === 12 ? 'primary' : 'secondary'}
+                            onClick={() => {setItemWidth(12)}}
+                        >
+                            List
+                        </Button>
+                    </div>
+                </div>
+                <Grid container spacing={4}>
+                    <Grid item xs={itemWidth}>
+                        <h2 style={{border: '1px solid black'}}>Item</h2>
+                    </Grid>
+                    <Grid item xs={itemWidth}>
+                        <h2 style={{border: '1px solid black'}}>Item</h2>
+                    </Grid>
+                    <Grid item xs={itemWidth}>
+                        <h2 style={{border: '1px solid black'}}>Item</h2>
+                    </Grid>
+                    <Grid item xs={itemWidth}>
+                        <h2 style={{border: '1px solid black'}}>Item</h2>
+                    </Grid>
+                    <Grid item xs={itemWidth}>
+                        <h2 style={{border: '1px solid black'}}>Item</h2>
+                    </Grid>
+                    <Grid item xs={itemWidth}>
+                        <h2 style={{border: '1px solid black'}}>Item</h2>
+                    </Grid>
+                </Grid>
             </div>
-            <Grid container spacing={4}>
-                <Grid item xs={itemWidth}>
-                    <h2 style={{border: '1px solid black'}}>Item</h2>
-                </Grid>
-                <Grid item xs={itemWidth}>
-                    <h2 style={{border: '1px solid black'}}>Item</h2>
-                </Grid>
-                <Grid item xs={itemWidth}>
-                    <h2 style={{border: '1px solid black'}}>Item</h2>
-                </Grid>
-                <Grid item xs={itemWidth}>
-                    <h2 style={{border: '1px solid black'}}>Item</h2>
-                </Grid>
-                <Grid item xs={itemWidth}>
-                    <h2 style={{border: '1px solid black'}}>Item</h2>
-                </Grid>
-                <Grid item xs={itemWidth}>
-                    <h2 style={{border: '1px solid black'}}>Item</h2>
-                </Grid>
-            </Grid>
-        </div>
+        </ThemeProvider>
     );
 };
 
