9e54c4a6736a55ad0f1f6c322b91195b1258936b
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2022-03-24 10:02:46 +0100

    Update category api routes

--- auction-app-frontend/src/services/ProductService.js
+++ auction-app-frontend/src/services/ProductService.js
@@ -1,11 +1,11 @@
 import api from 'services/Api';
 
 function getProducts(page, size, sortKey, sortDirection) {
-    return api.get(`/products?page=${page}&size=${size}&sortKey=${sortKey}&sortDirection=${sortDirection}`);
+    return api.get(`/product/get-all?page=${page}&size=${size}&sortKey=${sortKey}&sortDirection=${sortDirection}`);
 }
 
 function getProductById(id) {
-    return api.get(`/products/${id}`);
+    return api.get(`/product/get-one/${id}`);
 }
 
 const ProductService = {
